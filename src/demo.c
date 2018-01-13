2c394114b26e99ba555645e929c6444cc63d30c1
Yummy Bian <yummy.bian@gmail.com>
2018-01-13 14:02:37 +0800

    Free VideoWriter only when it's not empty.

--- src/demo.c
+++ src/demo.c
@@ -214,7 +214,9 @@ void demo(char *cfgfile, char *weightfile, float thresh, int cam_index, const ch
         pthread_join(detect_thread, 0);
         ++count;
     }
-    cvReleaseVideoWriter(&output_video);
+    if (output_video) {
+        cvReleaseVideoWriter(&output_video);
+    }
 }
 
 void demo_compare(char *cfg1, char *weight1, char *cfg2, char *weight2, float thresh, int cam_index, const char *filename, char **names, int classes, int delay, char *prefix, int avg_frames, float hier, int w, int h, int frames, int fullscreen)
